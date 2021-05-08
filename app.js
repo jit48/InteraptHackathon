d0add0480d5aae9a878b9e62b84ccecf06a15d61
unknown <shashank080311@gmail.com>
2021-05-08 12:21:58 +0530

    first commit in my branch

--- app.js
+++ app.js
@@ -39,22 +39,22 @@ const userSchema = new mongoose.Schema({
         projectStartDate: String,
         projectEndDate:String
     },
-    transferable:Boolean,
+    transferrable:Boolean,
 });
 
 const Project = mongoose.model("Project", projectSchema);
 const User = mongoose.model("User", userSchema);
 
 
-const project = new Project({
-     projectId:"1f6aea75-fca3-4b2f-b87b-fb71b5c8e21a",
-    projectName: "Alpino Jalpino",
-    description:"This is a short project",
-    startDate:"2021-02-21",
-    endDate:"2021-12-03",
-    totalResources:3,
-    location:"AZ",
-})
+// const project = new Project({
+//      projectId:"1f6aea75-fca3-4b2f-b87b-fb71b5c8e21a",
+//     projectName: "Alpino Jalpino",
+//     description:"This is a short project",
+//     startDate:"2021-02-21",
+//     endDate:"2021-12-03",
+//     totalResources:3,
+//     location:"AZ",
+// })
 
 // project.save();
 
@@ -66,6 +66,35 @@ app.get("/", function(req, res){
     res.render("index");
 })
 
+
+app.get("/dashboard",(req,res)=>{
+res.render("dashboard");
+});
+
+app.get("/projectData",(req,res)=>{
+    Project.find({},(err,foundProjects)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.json(foundProjects);
+        }
+    })
+})
+
+app.get("/projects/:projectId",(req,res)=>{
+    const reqProj = req.params.projectId;
+    User.find({projectId: reqProj},(err,foundProject)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("project",{foundProject})
+        }
+    })
+})
+
 app.listen(3000 , ()=>{
     console.log("server running at port 3000");
 });
+
+
+// "https://xd.adobe.com/view/26bb4dd1-2368-4f71-bf5f-4d7927e93c17-73e8/screen/dbab38c8-e61c-4720-92e2-c5536bfb53af/specs/"
---
+++ public/js/dashboard.js
@@ -0,0 +1,154 @@
+const search = document.getElementById('find');
+const empSearch = document.getElementById('empSearch');
+const projSearch = document.getElementById('projSearch');
+const projResults = document.getElementById('projResults');
+const empResults = document.getElementById('empResults');
+const Results = document.getElementById('Results');
+let searchText = search.value;
+window.addEventListener('load',()=>{
+    data();
+})
+   
+// search.addEventListener('click',()=>{
+//     search.classList.toggle('extendWidth');
+// })
+
+// empSearch.addEventListener('click', ()=>{
+//     empResults.classList.remove('displayNone')
+//     projResults.classList.add('displayNone')
+//     empData();
+//     matches = [];
+//     async function getEmpData(searchText){
+//         const res = await fetch('http://localhost:3000/employees');
+//         const data = await res.json();
+//         let matches = data.filter(d => {
+//             const regex = new RegExp(`^${searchText.toString()}`,'gi');
+//             return d.firstName.match(regex) ||  d._id.match(regex) || d.location.match(regex) || d.vendor.match(regex) 
+//         })
+//         Results.classList.add('displayNone');
+//         outputHtml(matches);
+//     }
+    
+//     search.addEventListener('input', ()=>{
+//         getEmpData(search.value)});
+
+// })
+
+projSearch.addEventListener('click', ()=>{
+    projResults.classList.remove('displayNone');
+    empResults.classList.add('displayNone');
+    data();
+    matches = []
+    async function getProjData(searchText){
+        console.log(searchText);
+        const res = await fetch('http://localhost:3000/projectData');
+        const data = await res.json();
+        let matches = data.filter(d => {
+            const regex = new RegExp(`^${searchText.toString()}`,'gi');
+            return d.projectName.match(regex) || d.projectId.match(regex) || d.location.match(regex)
+        })
+        console.log(matches);
+        Results.classList.add('displayNone');
+        outputHtmlProject(matches);
+    }
+    search.addEventListener('input', ()=>{
+        betterFunction()});
+
+        const getDebouncedData = function(fn,d){
+            let timer
+            return function(){
+                 clearInterval(timer)
+                timer = setTimeout(()=>{
+                     getProjData(search.value)
+                 },d)
+            }
+        }
+    const betterFunction = getDebouncedData(getProjData,300)
+ 
+})
+
+
+// function outputHtml(matches){
+//     if(matches.length > 0){
+//         const html = matches.map(match => (
+//             `<div class="projectCard">
+//                 <div class="projectDetails">
+//                     <p>${match.firstName}</p>
+//                     <p>${match.location}</p>
+//                 </div>
+//                 <div class="projectStartDate">
+//                     <p>${match.role}</p>
+//                 </div>
+//                 <div class="button">
+//                     <a href="/projects/${match.projectId}">explore</a>
+//                 </div>
+//             </div>`
+//         )).join('');
+//         empResults.innerHTML = html;
+//     }else{
+//         const html = '';
+//         empResults.innerHTML = html;
+//     }
+// }
+function outputHtmlProject(matches){
+    if(matches.length > 0){
+        const html = matches.map(match => (
+            `<div class="projectCard">
+                <div class="projectDetails">
+                    <p>${match.projectName}</p>
+                    <p>${match.location}</p>
+                </div>
+                <div class="projectStartDate">
+                    <p>${match.startDate}</p>
+                </div>
+                <div class="button">
+                    <a href="/projects/${match.projectId}">explore</a>
+                </div>
+            </div>`
+        )).join('');
+        projResults.innerHTML = html;
+    }else{
+        const html = '';
+        projResults.innerHTML = html;
+    }
+}
+async function data(){
+    const res = await fetch('http://localhost:3000/projectData');
+    const data = await res.json();
+    console.log(data);
+    const html = data.map(d => (
+        `<div class="projectCard">
+            <div class="projectDetails">
+                <p>${d.projectName}</p>
+                <p>${d.location}</p>
+            </div>
+            <div class="projectStartDate">
+                <p>${d.startDate}</p>
+            </div>
+            <div class="button">
+                <a href="/projects/${d.projectId}">explore</a>
+            </div>
+        </div>`
+    )).join('');
+        Results.innerHTML = html
+}
+// async function empData(){
+//     const res = await fetch('http://localhost:3000/employees');
+//     const data = await res.json();
+//     console.log(data);
+//     const html = data.map(d => (
+//         `<div class="projectCard">
+//         <div class="projectDetails">
+//             <p>${d.firstName}</p>
+//             <p>${d.location}</p>
+//         </div>
+//         <div class="projectStartDate">
+//             <p>${d.role}</p>
+//         </div>
+//         <div class="button">
+//             <a href="/projects/${d.projectId}">explore</a>
+//         </div>
+//     </div>`
+// )).join('');
+//     Results.innerHTML = html
+// }
